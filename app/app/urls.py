74449d89bc969cab76c72fd7dbec6881c0f2efee
elcobalto <crisep95@gmail.com>
2022-11-04 15:39:46 -0300

    feat: add authentication token

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,8 +17,9 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs'
-    )
+        name='api-docs',
+    ),
+    path('api/user/', include('user.urls'))
 ]
